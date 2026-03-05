47e0561591125212362d9bbf08e3afd43f7b19fa
jhair pinto <jhairpinto8@gmail.com>
2026-03-04 20:41:14 -0400

    inicio de login

---
+++ src/app/components/login/login.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.html',
+  styleUrls: ['./login.scss']
+})
+export class Login {
+
+ui = {
+
+opacity: 0.9,
+
+blur: 12,
+
+scale: 1,
+
+radius: 16,
+
+buttonColor: "#4c7dff"
+
+}
+
+}
